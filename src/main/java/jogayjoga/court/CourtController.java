5f12cb91630ee39974809aa153ba2a45879d362f
GustavoAntony <gustavoa9@al.insper.edu.br>
2024-04-05 17:41:49 -0300

    v1

--- src/main/java/jogayjoga/court/CourtController.java
+++ src/main/java/jogayjoga/court/CourtController.java
@@ -4,31 +4,31 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
-import java.util.List;
+// import java.util.List;
 
 
-@FeignClient("ingressify-court")
+@FeignClient("jogayjoga-court")
 public interface CourtController {
     
     @PostMapping("/court/")
     ResponseEntity<?> create(
-            @RequestBody(required = true) RegisterIn in);
+            @RequestBody(required = true) CourtIn in);
     
-    @GetMapping("/court/")
-    //list?
-    ResponseEntity<List<CourtOut>> getAllCourts();
+    // @GetMapping("/court/")
+    // //list?
+    // ResponseEntity<List<CourtOut>> getAllCourts();
 
     @GetMapping("/court/{id}")
     ResponseEntity<CourtOut> get(
         @PathVariable(required = true) String id
     );
         
-    @PutMapping("/court/{id}")
-    ResponseEntity<CourtOut> update (
-        @PathVariable(required = true) String id,
-        @RequestBody(required = true) UpdateIn in
-    );
+    // @PutMapping("/court/{id}")
+    // ResponseEntity<CourtOut> update (
+    //     @PathVariable(required = true) String id,
+    //     @RequestBody(required = true) UpdateIn in
+    // );
 }
--- src/main/java/jogayjoga/court/CourtOut.java
+++ src/main/java/jogayjoga/court/CourtOut.java
@@ -3,6 +3,7 @@ import lombok.Builder;
 
 @Builder
 public record CourtOut(
+    String id,
     String name,
     String address
 )
