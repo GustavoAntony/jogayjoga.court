729fd44574550a03125482e9d2a7c8a2500a0f6f
eriksoaress <erikbtu2020@gmail.com>
2024-05-25 16:46:16 -0300

    correcoes

--- src/main/java/jogayjoga/court/CourtOut.java
+++ src/main/java/jogayjoga/court/CourtOut.java
@@ -1,12 +1,14 @@
 package jogayjoga.court;
 import lombok.Builder;
+import java.io.Serializable;
 
 @Builder
 public record CourtOut(
     String id,
     String name,
     String address
-)
+) implements Serializable
 {
+    private static final long serialVersionUID = 1L;
     
 }
